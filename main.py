d50704174fa9e2dbaa0ba0cabca224be2a5ac85c
Jeffin George <jeffingn@gmail.com>
2023-04-07 14:26:40 +0000

    x

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
 
 def get_driver():
-  options= webdriver.ChromeOptions()
-  options.add_argument("disable-infobars")
-  options.add_argument("start-maximized")
-  options.add_argument("disable-dev-shm-usage")
-  options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches",["enable-automation"])
-  options.add_experimental_option("disable-blink-features=AutomationControlled")
+  x= webdriver.ChromeOptions()
+  x.add_argument("disable-infobars")
+  x.add_argument("start-maximized")
+  x.add_argument("disable-dev-shm-usage")
+  x.add_argument("no-sandbox")
+  x.add_experimental_option("excludeSwitches",["enable-automation"])
+  x.add_argument("disable-blink-features=AutomationControlled")
 
-driver = webdriver.Chrome(options)
-driver.get("http://automated.pythonanywhere.com")
-return driver
+  driver = webdriver.Chrome(options=x)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
 
 def main():
   driver=get_driver()
-  element=driver.find_element_by_xpath("/html/body/div[1]/div/h1[1]")
+  element=driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
+
