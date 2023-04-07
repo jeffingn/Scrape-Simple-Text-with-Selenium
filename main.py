6505f582b6fbfbf9a0dbc7cb2af62f46301a005c
Jeffin George <jeffingn@gmail.com>
2023-04-07 12:04:48 +0000

    f

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import pandas
-import calculate
+from selenium import webdriver
 
+def get_driver():
+  options= webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",["enable-automation"])
+  options.add_experimental_option("disable-blink-features=AutomationControlled")
 
-print('jefin')
-print('george')
+driver = webdriver.Chrome(options)
+driver.get("http://automated.pythonanywhere.com")
+return driver
 
-def foo(x):
-  return x
-
-def jeff(x):
-  return(x)
-
-def geo(x):
-  return (x)
+def main():
+  driver=get_driver()
+  element=driver.find_element_by_xpath("/html/body/div[1]/div/h1[1]")
